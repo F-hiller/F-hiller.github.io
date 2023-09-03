1b38b65b4225567af09443540801661f66bba50e
F-hiller <jjun6912@gmail.com>
2023-09-03 21:51:23 +0900

    update posts

--- post/make.sh
+++ post/make.sh
@@ -17,9 +17,9 @@ done
 # markdown 파일을 post 폴더 안에 생성합니다.
 cat << EOF > "$filename"
 ---
-code_numbers: 
-language: 
-title: 
+code_numbers: a b
+language: c
+title: d
 date: $(date +"%Y-%m-%d %H:%M:%S %z")
 categories: []
 tags: []
@@ -35,6 +35,11 @@ tags: []
 ## 풀이
 
 ### 코드
+/*
+[a] d - b (c) 의 형식으로 title이 작성된다. 수식을 사용하는 게시글의 경우는 use_math 주석을 해제하고 작성하면 된다.
+카테고리는 대문자로 시작하고 태그는 소문자로 시작한다. "," 콤마로 구분하여 작성하면 된다. 
+프로그래머스 링크처럼 미리보기 이미지 등이 갖춰져있는 경우 linkpreview를, 그냥 링크만 달아두는 경우는 markdown 문법을 사용하면 된다.
+*/
 EOF
 
 echo "[Success] : File $filename has been created!"
